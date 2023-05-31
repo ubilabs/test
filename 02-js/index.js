f95872837aa44d164824a0daa46b6c7c475469d7
Martin Kleppe <kleppe@ubilabs.net>
2023-05-31 13:13:46 +0200

    Update index.js

--- 02-js/index.js
+++ 02-js/index.js
@@ -4,7 +4,7 @@ function zeroFill( number, width )
  width -= number.toString().length;
  if ( width > 0 )
  {
-   return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + ber;
+   return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
  }
  return number + ""; // always return a string
 }
