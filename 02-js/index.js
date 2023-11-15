02d8886fbeadfb5fe79c70ac9174180b2c19b60b
ArvedBoetefuer <arvedb96@gmx.de>
2023-11-15 17:58:16 +0100

    Worked on task 1-3, Task 4 had an API-key Errror

--- 02-js/index.js
+++ 02-js/index.js
@@ -14,11 +14,31 @@ function zeroFill( number, width )
 
 Code review:
 
-- 
-- 
+- line 2, what is number? what is width?
+- line 2, method name does not fully conclude what happends in itself or write a more explaining comment.
+- line 3 would open method after parameters in line 2.
+- line 4-8 styleguide. Move method body to the right.
+- line 7, actually never seen something like that. What is '/\./.test'?
 - 
 
 */
 
 
 // your solution:
+
+/*
+@Method zeroFillIntergerAsString
+This method aims to return a String, wich is constructed by to intergers. 
+first one beeing 'numberstoZeroFill' and lenghOfZeroFilling.
+@param 'numberToZeroFill' the number we want to turn into a string with X leading Zeros
+@param 'lenghOfZeroFilling' the total lengh of the string we want to return.
+@return String 'zeroFilledNumber'
+*/
+function zeroFillIntergerAsString(numberToZeroFill, lenghOfZeroFilling){
+
+    //.padStart() method is used to pad a number with leading zeros. It takes two parameters,
+    // the first one is the total length of the string, and the other one is the string or character the user wants to use for padding.
+    let zeroFilledNumber = numberToZeroFill.toString().padStart(lenghOfZeroFilling,'0');
+
+    return zeroFilledNumber;  
+}
